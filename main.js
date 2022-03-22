fe05c5dac54b9f74f00d37c41e415a2f6fe673c3
alexpagani <alexpaganibr@gmail.com>
2022-03-22 15:33:04 +0100

    inserimento arrotondamento del prezzo

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const cognome = prompt("inserisci il tuo cognome");
 const eta = prompt("Quanti anni hai ?");
 const kilometri = prompt("Quanti kilometri deve percorrere ?");
 
+function round(num) {
+    let m = Number((Math.abs(num) * 100).toPrecision(15));
+    return Math.round(m) / 100 * Math.sign(num);
+}
+
 //prezzo del biglietto
 let sconto;
 let prezzo = 0.21 * kilometri;
@@ -14,15 +19,18 @@ if(eta <= 17){
     
     sconto = (prezzo * 20)/100;
     prezzo = prezzo - sconto;
+    prezzo = round(prezzo);
     document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
 }
 else if(eta > 65){
     
     sconto = (prezzo * 40)/100;
     prezzo = prezzo - sconto;
+    prezzo = round(prezzo);
     document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
 }
 else{
+    prezzo = round(prezzo);
     document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
 }
 
