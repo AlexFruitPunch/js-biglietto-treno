b3f8f55b3163a2e86007a0f245bee49577ff4456
alexpagani <alexpaganibr@gmail.com>
2022-03-22 14:35:20 +0100

    Prima impostazione del codice

---
+++ main.js
@@ -0,0 +1,14 @@
+//introduzione alla pagina
+prompt("Benvenuto nel nostro servizio di biglietteria !");
+
+//inserimento dati personali per il biglietto
+const nome = prompt("inserisci il tuo nome");
+const cognome = prompt("inserisci il tuo cognome");
+const eta = prompt("Quanti anni hai ?");
+const kilometri = prompt("Quanti kilometri deve percorrere ?");
+
+//prezzo del biglietto
+
+
+
+
