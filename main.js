05f14c792789556c8595984cce7e81763f1cf3e8
alexpagani <alexpaganibr@gmail.com>
2022-03-22 15:28:04 +0100

    inserimento prezzo nella pagina html con sconti

--- main.js
+++ main.js
@@ -8,7 +8,25 @@ const eta = prompt("Quanti anni hai ?");
 const kilometri = prompt("Quanti kilometri deve percorrere ?");
 
 //prezzo del biglietto
+let sconto;
+let prezzo = 0.21 * kilometri;
+if(eta <= 17){
+    
+    sconto = (prezzo * 20)/100;
+    prezzo = prezzo - sconto;
+    document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
+}
+else if(eta > 65){
+    
+    sconto = (prezzo * 40)/100;
+    prezzo = prezzo - sconto;
+    document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
+}
+else{
+    document.getElementById("PrezzoBiglietteria").innerHTML = prezzo; 
+}
 
-
-
+//dati biglietto
+document.getElementById("Nome").innerHTML = nome;
+document.getElementById("Cognome").innerHTML = cognome;
 
